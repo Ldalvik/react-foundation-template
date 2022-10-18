eaa6fa07b663e6dbb4705ae6248afbe22d74756c
julianosborne <subaruforge@gmail.com>
2022-10-17 22:49:29 -0400

    Added grid-x and padding

--- src/components/GridContainer.js
+++ src/components/GridContainer.js
@@ -1,9 +1,11 @@
 const GridContainer = ({ children }) => {
-    return (
-        <div className="grid-container"> {/* "grid-container fluid" for full page width*/}
-            {children}
+  return (
+    <div className="grid-container"> {/* "grid-container fluid" for full page width*/}
+      <div className="grid-x grid-padding-x grid-padding-y">
+          {children}
         </div>
-    )
-}
+    </div>
+  );
+};
 
-export default GridContainer
+export default GridContainer;
